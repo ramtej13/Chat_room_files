836c195d6040ad88083bd0bac985a9a89b94278b
ramtej13 <ramtej.j13@gmail.com>
2020-09-17 01:20:57 +0530

    new

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zs-v((4k##%&u0=k$@2j)%$m()e%3-h7jul@e0v$3!8k08j#rb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.72.78.36','127.0.0.1']
+ALLOWED_HOSTS = ['34.70.206.233','127.0.0.1']
 
 
 # Application definition
